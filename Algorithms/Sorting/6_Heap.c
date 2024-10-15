659c8f72893709762b1ea36ee6a656145e65d571
Nam <hoangnhatnamkiller21092k5@gmail.com>
2024-10-16 00:19:40 +0700

    Heap sort

--- Algorithms/Sorting/6_Heap.c
+++ Algorithms/Sorting/6_Heap.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// The defination of the max heap mean that: every root of child heap has the biggest value
+// Don't be confuse that max heap mean it is the aranged array like: 8 7 6 5 4 3 2 1 0
+// It can be like: 8 6 7 4 5 2 3 1 0 
+// When displaying with form of tree, the tree has to be read from left to right
+
 // Function to maintain the heap property
 void heapify(int A[], int i, int N) {
+    // N is the number of elements in the heap (Size of the heap)
+    // i is the index of the root of the heap
     int L = 2 * i + 1; // Left child (adjusted for 0-based indexing)
     int R = 2 * i + 2; // Right child (adjusted for 0-based indexing)
     int max = i;
